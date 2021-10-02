fa9ec59a11c7a30db7b57fe203b1c53f7173d7be
Joshua Pearson <jcpearson01@gmail.com>
2021-10-02 16:08:30 +0100

    Creation of a receivelog

--- Practice/receive.py
+++ Practice/receive.py
@@ -4,6 +4,7 @@ import board
 import busio
 import digitalio
 import adafruit_rfm9x
+from datetime.datetime import now
 
 # Set radio frequency, in MHz
 # 915.0 for US, 868.0 for UK/Europe
@@ -22,16 +23,28 @@ rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
 # Set power of transceiver. Max value is 23dB.
 rfm9x.tx_power = 23
 
+def wrt(message):
+    """
+    Opens a CSV file, writes a message, closes
+    Prints to terminal
+    """
+    log = open('receivelog.csv', 'a') 
+    log.write(message)
+    log.close()
+    print(message)
+
 while True:
     packet = rfm9x.receive(timeout=1.0)
     # If no packet was received during the timeout then None is returned.
+    tm = str(now())
     if packet is None:
         # Packet has not been received
-        print("Null")
+        message = "{0}, Null, 0, 0, 0". format(tm)
     else:
         # Packet received
         # Print out the raw bytes of the packet, the decoded ASCII and the signal strength
         # Make sure it is being sent in ASCII!
         packet_text = str(packet, "ascii")
         rssi = rfm9x.last_rssi
-        print("Received, {0}, {1}, {2}".format(packet, packet_text, rssi))
+        message = "{0}, Received, {1}, {2}, {3}".format(tm, packet, packet_text, rssi)
+    wrt(message)

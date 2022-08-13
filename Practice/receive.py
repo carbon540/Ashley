f2e9da59dfd3952c027a150c98d720998b7052e7
Alex Peake <lexpeake@gmail.com>
2022-08-13 17:33:08 +0100

    commented out coding rate

--- Practice/receive.py
+++ Practice/receive.py
@@ -23,27 +23,27 @@ rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
 # SETTINGS of transceiver. These can be altered to improve range or bitrate
 
 # Power of transceiver. Max value is 23dB.
-rfm9x.tx_power = 18
+rfm9x.tx_power = 23
 
 # Spreading factor. 
 # Valid settings are (6, 7, 8, 9, 10, 11, 12)
 # Higher settings improve range and decrease rate
-rfm9x.spreading_factor = 12
+# rfm9x.spreading_factor = 12
 
 # Signal bandwidth
 # Valid settings are (7800, 10400, 15600, 20800, 31250, 41700, 62500, 125000, 250000)
 # Higher values increase throughput
-rfm9x.signal_bandwidth = 7800
+# rfm9x.signal_bandwidth = 7800
 
 # Coding rate
 # Valid settings are (5, 6, 7, 8)
 # Level of redundency - higher values are more interference-tolerant
-rfm9x.coding_rate = 8
+# rfm9x.coding_rate = 8
 
 # Enable CRC (cyclic redundency checker)
 # Valid settings are (True, False)
 # Increases redundency
-rfm9x.enable_crc = True
+# rfm9x.enable_crc = True
 
 def wrt(message):
     """

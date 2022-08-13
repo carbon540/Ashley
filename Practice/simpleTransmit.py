32cf08677d778b1b5d1f36f617cbaf7381aff31e
Joshua Pearson <jcpearson01@gmail.com>
2022-08-13 16:35:40 +0100

    simpleTransmit created

---
+++ Practice/simpleTransmit.py
@@ -0,0 +1,55 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Simple demo of sending and recieving data with the RFM95 LoRa radio.
+# Author: Tony DiCola
+import board
+import busio
+import digitalio
+
+import adafruit_rfm9x
+
+from numpy import random
+import time
+
+# Define radio parameters.
+RADIO_FREQ_MHZ = 868.0  # Frequency of the radio in Mhz. Must match your
+# module! Can be a value like 915.0, 433.0, etc.
+
+# Define pins connected to the chip, use these if wiring up the breakout according to the guide:
+CS = digitalio.DigitalInOut(board.CE1)
+RESET = digitalio.DigitalInOut(board.D25)
+# Or uncomment and instead use these if using a Feather M0 RFM9x board and the appropriate
+# CircuitPython build:
+# CS = digitalio.DigitalInOut(board.RFM9X_CS)
+# RESET = digitalio.DigitalInOut(board.RFM9X_RST)
+
+# Define the onboard LED
+LED = digitalio.DigitalInOut(board.D13)
+LED.direction = digitalio.Direction.OUTPUT
+
+# Initialize SPI bus.
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+# Initialze RFM radio
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+# Note that the radio is configured in LoRa mode so you can't control sync
+# word, encryption, frequency deviation, or other settings!
+
+# You can however adjust the transmit power (in dB).  The default is 13 dB but
+# high power radios like the RFM95 can go up to 23 dB:
+rfm9x.tx_power = 23
+
+while True:
+    # Generate a number to send
+    x = str(random.rand())
+
+    # Send a packet.  Note you can only send a packet up to 252 bytes in length.
+    # This is a limitation of the radio packet size, so if you need to send larger
+    # amounts of data you will need to break it into smaller send calls.  Each send
+    # call will wait for the previous one to finish before continuing.
+    rfm9x.send(bytes(x, "utf-8"))
+    print("Sent", x)
+    
+    time.sleep(1)

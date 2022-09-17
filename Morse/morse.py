647beda952e17e45f539911a8e3952103c24f78f
ADPeake <lexpeake@gmail.com>
2022-09-17 18:02:56 +0100

    rough start on morse code

---
+++ Morse/morse.py
@@ -0,0 +1,22 @@
+import board
+import busio
+import digitalio
+import adafruit_rfm9x
+from datetime import datetime
+import time
+from alphabet import morseAlphabet
+
+send_buffer = []
+receive_buffer = []
+GPIO_DOT = 5
+GPIO_DASH = 6
+
+if __name__ =='__main__':
+    raise NotImplementedError()
+    # 1.0 start listening for new packets
+        # 1.1 run check on previous  8 characters stored in buffer
+        # 1.2 write new characters to display
+    # 2.0 send packets
+
+    
+   
